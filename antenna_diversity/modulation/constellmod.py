bbd41f90ed416542b60216c827d4fb274b354f36
Julian T <julian@jtle.dk>
2021-04-11 11:31:19 +0200

    Remove gray encoding from modulation

--- antenna_diversity/modulation/constellmod.py
+++ antenna_diversity/modulation/constellmod.py
@@ -1,8 +1,4 @@
-from .. import encoding
-
 import numpy as np
-import typing as t
-
 import matplotlib.pyplot as plt
 
 
@@ -14,24 +10,16 @@ class ConstellationModulator:
     When extending, one must implement the "generate_constellation()" method
     """
 
-    def __init__(self, M: int, use_gray: bool = True, energy: float = 1) -> None:
+    def __init__(self, M: int, energy: float = 1) -> None:
         # Set members
         self.M = M
-
-        # Use gray encoder if user wants it
-        self.encoder: t.Any = None
-        if use_gray:
-            self.encoder = encoding.GrayEncoder(M)
-        else:
-            self.encoder = encoding.NoEncoder()
 
         # Calculate the constellation
         self.constellation = np.array(self.generate_constellation(energy))
 
     def modulate(self, symbols: np.ndarray) -> np.ndarray:
         # make d scale mean symbol energy
-        encoded = self.encoder.encode(symbols)
-        return self.constellation[encoded]
+        return self.constellation[symbols]
 
     def demodulate(self, received: np.ndarray) -> np.ndarray:
         # There is a column for each constellation element and a row for each
@@ -40,9 +28,9 @@ class ConstellationModulator:
         # compare elements in a row as opposed to in a column.
         distances = np.abs(np.subtract.outer(received, self.constellation))
         estimated_symbols = np.argmin(distances, 1)
-        return self.encoder.decode(estimated_symbols)
+        return estimated_symbols
 
-    def generate_constellation(self, energy: float) -> np.ndarray:
+    def create_constellation(self, energy: float) -> np.ndarray:
         # Just create a dummy constellation
         raise Exception(f"Dummy constellation generator not overwritten by {self.__class__.__name__}")
         return np.zeros(self.M)
