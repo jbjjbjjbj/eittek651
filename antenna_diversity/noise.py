bf499924b48c5f4f1862a031d20dcec9e807ec03
cspe18 <47355558+cspe18@users.noreply.github.com>
2021-04-12 13:58:21 +0200

    remove hello and added spaces

--- antenna_diversity/noise.py
+++ antenna_diversity/noise.py
@@ -3,8 +3,8 @@ import math
 from .common import db_to_power
 
 def AWGN(x,snr):
-    normsize=len(x)
-    sigma=math.sqrt((1/db_to_power(snr))/2)
-    W=np.random.normal(size=normsize)*sigma + 1j*np.random.normal(size=normsize)*sigma
-    y=x+W
+    normsize = len(x)
+    sigma = math.sqrt((1/db_to_power(snr))/2)
+    W = np.random.normal(size=normsize)*sigma + 1j*np.random.normal(size=normsize)*sigma
+    y = x + W
     return y
