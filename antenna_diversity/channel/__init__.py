4fe294def35aede058166c5e64d84d2a80e95be5
cspe18 <47355558+cspe18@users.noreply.github.com>
2021-04-13 10:27:48 +0200

    minor changes

--- antenna_diversity/channel/__init__.py
+++ antenna_diversity/channel/__init__.py
@@ -1,2 +1,3 @@
 from .noise import AWGN
 from .channelsim import rayawgnchannel
+from .fading import rayleigh
--- antenna_diversity/channel/fading.py
+++ antenna_diversity/channel/fading.py
@@ -1,8 +1,7 @@
 import numpy as np
 import math
 
-
-def rayawgnchannel(sizeofinput):
+def rayleigh(sizeofinput):
     raysize = len(sizeofinput) #unsure if this is correct
     alpha = np.random.rayleigh(size = raysize,scale = 1/math.sqrt(2))
     return alpha
