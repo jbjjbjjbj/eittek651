31fd821595f3ac86718e237a45af301437dae3dc
Julian T <julian@jtle.dk>
2021-04-14 11:41:44 +0200

    Rename exp to expected

--- test/test_gray.py
+++ test/test_gray.py
@@ -6,15 +6,15 @@ from antenna_diversity.encoding import GrayEncoder
 class TestGrayEncoder(unittest.TestCase):
     def setUp(self):
         self.inputs = np.array([1, 3, 2, 1, 0, 0])
-        self.exp = np.array([1, 2, 3, 1, 0, 0])
+        self.expected = np.array([1, 2, 3, 1, 0, 0])
         pass
 
     def test_encode(self):
         encoded = GrayEncoder(4).encode(self.inputs)
-        self.assertTrue(np.array_equal(encoded, self.exp))
+        self.assertTrue(np.array_equal(encoded, self.expected))
 
     def test_decode(self):
-        decoded = GrayEncoder(4).encode(self.exp)
+        decoded = GrayEncoder(4).encode(self.expected)
         self.assertTrue(np.array_equal(decoded, self.inputs))
 
     def test_invalid_m(self):
--- test/test_symbolencoder.py
+++ test/test_symbolencoder.py
@@ -6,11 +6,11 @@ from antenna_diversity.encoding import SymbolEncoder
 class TestSymbolEncoder(unittest.TestCase):
     def setUp(self):
         self.inputs = bytes.fromhex("DEADBEEF")
-        self.exp2 = np.array([
+        self.expected2 = np.array([
             1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
             1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
             ])
-        self.exp4 = np.array([
+        self.expected4 = np.array([
             3, 1, 3, 2, 2, 2, 3, 1, 2, 3, 3, 2, 3, 2, 3, 3
             ])
 
@@ -18,17 +18,17 @@ class TestSymbolEncoder(unittest.TestCase):
         self.assertTrue(np.array_equal(a, b))
 
     def test_encode_m2(self):
-        self.assertNP(SymbolEncoder(2).encode_msb(self.inputs), self.exp2)
+        self.assertNP(SymbolEncoder(2).encode_msb(self.inputs), self.expected2)
 
     def test_m3(self):
         with self.assertRaisesRegex(Exception, "SymbolEncoder created with"):
             SymbolEncoder(3)
 
     def test_encode_m4(self):
-        self.assertNP(SymbolEncoder(4).encode_msb(self.inputs), self.exp4)
+        self.assertNP(SymbolEncoder(4).encode_msb(self.inputs), self.expected4)
 
     def test_decode_m2(self):
-        self.assertNP(SymbolEncoder(2).decode_msb(self.exp2), self.inputs)
+        self.assertNP(SymbolEncoder(2).decode_msb(self.expected2), self.inputs)
 
     def test_decode_m4(self):
-        self.assertNP(SymbolEncoder(4).decode_msb(self.exp4), self.inputs)
+        self.assertNP(SymbolEncoder(4).decode_msb(self.expected4), self.inputs)
