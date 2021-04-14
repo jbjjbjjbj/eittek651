a70755b47865426e967d21a2ea357d523ff15ab0
Julian T <julian@jtle.dk>
2021-04-14 15:35:41 +0200

    Add theoritical bit and symbol error rates to pam

--- antenna_diversity/common.py
+++ antenna_diversity/common.py
@@ -1,26 +1,26 @@
 """
 Defines som common functions and shorthand functions used across modules.
 """
-import math
+import numpy as np
 import scipy.stats
 
 
-def q_function(x: float) -> float:
+def q_function(x: np.ndarray) -> np.ndarray:
     """
     Returns the Q-function of x.
     """
     return 1-scipy.stats.norm.cdf(x)
 
 
-def db_to_power(decibel: float) -> float:
+def db_to_power(decibel: np.ndarray) -> np.ndarray:
     """
     Returns power value from a decibel value.
     """
     return 10**(decibel/10)
 
 
-def db_from_power(power: float) -> float:
+def db_from_power(power: np.ndarray) -> np.ndarray:
     """
     Returns decibel value from a power value.
     """
-    return 10*math.log(power, 10)
+    return 10*np.log10(power)
