766757b7840f2662405df7532fa83d0df4090258
Julian T <julian@jtle.dk>
2021-04-16 11:42:31 +0200

    Refactor error.py
    This breaks the error_rate_plots.py file

--- antenna_diversity/error.py
+++ antenna_diversity/error.py
@@ -2,62 +2,56 @@ import numpy as np
 import typing as t
 
 
-def count_bits(byte: int) -> int:
+def count_bits(val: int) -> int:
     """
     >>> count_bits(0b10101100)
     4
     >>> count_bits(0xFF)
     8
+    >>> count_bits(432432043213)
+    22
     >>> count_bits(0)
     0
     >>> count_bits(1)
     1
     """
     count = 0
-    for i in range(8):
-        count += byte & 1
-        byte = byte >> 1
+    for i in range(val.bit_length()):
+        count += val & 1
+        val = val >> 1
 
     return count
 
 
-class BitErrorMeasure:
-    def __init__(self, original: bytes, copy=False) -> None:
-        self.original = np.frombuffer(original, dtype=np.ubyte)
-        if copy:
-            self.original = np.copy(self.original)
-
-        self.n = len(self.original)
-
-        # Create bit count lookup table
-        self.bit_count_lookup = np.empty(256, dtype=int)
-        for i in range(256):
-            self.bit_count_lookup[i] = count_bits(i)
-
-    def check_against(self, other_raw: bytes) -> t.Tuple[float, int, int]:
-        other = np.frombuffer(other_raw, dtype=np.ubyte)
-
-        n = len(other)
-        if n != self.n:
-            raise Exception("array to be checked is not the original size")
-
-        total_bits = n * 8
-
-        difference = np.bitwise_xor(other, self.original)
-        counts = self.bit_count_lookup[difference]
-        wrong_bits = np.sum(counts)
-
-        return wrong_bits / total_bits, wrong_bits, total_bits
+# Create bit count lookup table
+bit_count_lookup = np.empty(256, dtype=int)
+for i in range(256):
+    bit_count_lookup[i] = count_bits(i)
 
 
-class SymErrorMeasure:
-    def __init__(self, original: np.ndarray, copy=False) -> None:
-        self.original = original
-        if copy:
-            self.original = np.copy(self.original)
+def count_bit_errors(a: bytes, b: bytes) -> t.Tuple[int, int]:
+    a_np = np.frombuffer(a, dtype=np.ubyte)
+    b_np = np.frombuffer(b, dtype=np.ubyte)
 
-    def check_against(self, other: np.ndarray) -> t.Tuple[float, int, int]:
-        total = len(other)
-        wrong = np.sum(self.original != other)
+    n = len(a_np)
+    if n != len(b_np):
+        raise Exception("array to be checked is not the original size")
 
-        return wrong / total, wrong, total
+    total_bits = n * 8
+
+    difference = np.bitwise_xor(a_np, b_np)
+    counts = bit_count_lookup[difference]
+    wrong_bits = np.sum(counts)
+
+    return wrong_bits, total_bits
+
+
+def count_symbol_errors(a: np.ndarray, b: np.ndarray) \
+        -> t.Tuple[float, int, int]:
+
+    if len(a) != len(b):
+        raise Exception("array to be checked is not the original size")
+
+    wrong = np.sum(a != b)
+
+    return wrong, len(a)
--- test/test_error.py
+++ test/test_error.py
@@ -1,34 +1,32 @@
 import unittest
-from antenna_diversity.error import BitErrorMeasure, SymErrorMeasure
+from antenna_diversity import error
 import numpy as np
 
 
 class TestBitErrorMeasure(unittest.TestCase):
     def test(self):
         data = np.zeros(100, dtype=np.ubyte)
-
-        meas = BitErrorMeasure(data.tobytes())
+        data_before = np.copy(data)
 
         data[0] = 1
         data[50] = 0xFF
 
-        frac, faults, _ = meas.check_against(data)
+        faults, total = error.count_bit_errors(data, data_before)
 
-        self.assertEqual(frac, 0.01125)
         self.assertEqual(faults, 9)
+        self.assertEqual(total, 100 * 8)
 
 
 class TestSymErrorMeasure(unittest.TestCase):
     def test(self):
         data = np.arange(100) * 23
-
-        meas = SymErrorMeasure(data, copy=True)
+        data_before = np.copy(data)
 
         data[0] = 23
         data[23] = 12
         data[3] = 12
         data[99] = 12
 
-        frac, faults, _ = meas.check_against(data)
-        self.assertEqual(frac, 0.04)
+        faults, total = error.count_symbol_errors(data, data_before)
+        self.assertEqual(total, 100)
         self.assertEqual(faults, 4)
