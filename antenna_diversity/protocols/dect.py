8e8e3f8d06e258bfe072c4835bb3c20981a8d00c
hvisti <60644294+hvisti@users.noreply.github.com>
2021-04-19 11:28:40 +0200

    Restructured DECT class

--- antenna_diversity/protocols/dect.py
+++ antenna_diversity/protocols/dect.py
@@ -28,9 +28,18 @@ class DECT():
     def __init__(self, M) -> None:
         self.M = M
 
-    def full(self, payload):
-        return Full(payload, self.M)
-
+    def create_full(self, payload: bytes):
+        full = Full(self.M)
+        full.create_packet(payload)
+        return full
+    
+    def unpack_full(self, dect_packet: bytes):
+        full = Full(self.M)
+        full.from_bytes(dect_packet)
+        return full
+    
+    def check_crc_full(self, dect_packet: bytes):
+        return self.unpack_full(dect_packet).check_crc()
 
 class Full():
     """
@@ -45,33 +54,35 @@ class Full():
     4 bit Z-field (copy of X)
     """
 
-    def __init__(self, payload: bytes, M=2) -> None:
+    def __init__(self, M=2) -> None:
         if M != 2:
             raise Exception("Only M=2 is supported")
         self.a_field_format = '=B5sH'
+        self.packet_format = '=HL8s40sB'
+        
+    def create_packet(self, payload: bytes):
         self.a_header = int('1'*8, 2)
         self.a_tail = bytes.fromhex('AA' * int(40/8))
-        self.crc_fields = struct.pack('B5s', self.a_header, self.a_tail)
-        self.a_crc = cr.crc.Crc16DectR().calc(self.crc_fields)
+        crc_fields = struct.pack('B5s', self.a_header, self.a_tail)
+        self.a_crc = cr.crc.Crc16DectR().calc(crc_fields)
 
-        self.packet_format = '=HL8s40sB'
         self.preamble = int('10' * 8, 2)
         self.sync = int('1'*32, 2)
         if len(payload) != int(320/8):
             raise Exception(f"payload is not {int(320/8)} long")
         self.b_field = bytes(payload)
-        
-        
-        self.test_bytes = prepare_test_bytes(self.b_field)
 
+        self.xz_field = self.calculate_xz_field()
+        
+    
+    def calculate_xz_field(self):
+        test_bytes = prepare_test_bytes(self.b_field)
 
-        x_field = crc(self.test_bytes)  # Not sure if correct
-        print(x_field.bit_length())
+        x_field = crc(test_bytes)
         if x_field.bit_length() > 4:
             raise Exception("x_field bit length is not 4")
 
-        self.xz_field = x_field | (x_field << 4)
-    
+        return x_field | (x_field << 4)
     
     def to_bytes(self) -> bytes:
         a_field = struct.pack(self.a_field_format,
@@ -90,22 +101,16 @@ class Full():
     def from_bytes(self, bytes):
         self.preamble, self.sync, a_field, self.b_field, self.xz_field = \
             struct.unpack(self.packet_format, bytes)
+            
         self.a_header, self.a_tail, self.a_crc = \
             struct.unpack(self.a_field_format, a_field)
     
     def check_crc(self) -> bool:
-        self.test_bytes = prepare_test_bytes(self.b_field)
-
-        x_field = crc(self.test_bytes)  # Not sure if correct
-        
-        xz_field = x_field | (x_field << 4)
-        
-        return xz_field == self.xz_field
+        return self.calculate_xz_field() == self.xz_field
         
 
 
 def crc(data: bytes):
-
     a = bitarray()
     a.frombytes(data)
     a.extend([0,0,0,0])
