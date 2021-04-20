2d1e0d18b7fade49d10fdd535400c8693afcb166
vdot0x23 <40716069+vdot0x23@users.noreply.github.com>
2021-04-20 08:45:24 +0200

    Update examples/antenna_diversity.py
    Co-authored-by: Julian Teu <julian@jtle.dk>

--- examples/antenna_diversity.py
+++ examples/antenna_diversity.py
@@ -1,6 +1,5 @@
 # This pretends as if it were the antenna_diversity module
 import os
 from pathlib import Path
-filepath = os.path.realpath(__file__)
-os.chdir(Path(filepath).parent)
-import antenna_diversity
+filepath = Path(os.path.realpath(__file__))
+sys.path.append(filepath.parent)
