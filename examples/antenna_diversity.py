1f7177a6c45d3b644135394d9844926c6dd24770
Julian T <julian@jtle.dk>
2021-04-20 09:34:31 +0200

    Import sys in examples

--- examples/antenna_diversity.py
+++ examples/antenna_diversity.py
@@ -1,5 +1,7 @@
 # This pretends as if it were the antenna_diversity module
 import os
+import sys
 from pathlib import Path
+
 filepath = Path(os.path.realpath(__file__))
 sys.path.append(filepath.parent)
