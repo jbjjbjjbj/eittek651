1715007d81a9019e6cc0cfed241baead97302a27
Victor Büttner <victor@0x23.dk>
2021-04-20 11:22:17 +0200

    get new enough version of bitarray from mach-nix

--- shell.nix
+++ shell.nix
@@ -6,16 +6,20 @@ let
       ref = "refs/tags/3.2.0";
       rev = "ac62255e8112e547432ca0f09bfe8f4d1920fbb8";
     }
-  ) {};
+    ) {
+      pypiDataRev = "c67aa64b8cfb625a4ebf197d3e3edd9a6e3fccb3";
+      pypiDataSha256 = "0ma6naxawngbd4m2c35dywqa1vlw6ckfgp8q803j8kragy2ybbyl";
 
-  pips = mach-nix.mkPython {
-    requirements = builtins.readFile ./requirements.txt;
-  };
+    };
+
+    pips = mach-nix.mkPython {
+      requirements = builtins.readFile ./requirements.txt;
+    };
 in
-pkgs.mkShell {
-  buildInputs = [
-    pips
-    pkgs.python39
-  ];
+  pkgs.mkShell {
+    buildInputs = [
+      pips
+      pkgs.python39
+    ];
 
-}
+  }
