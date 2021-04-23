48547d4d324a7180360227a5ebf25aed00f7d609
Julian T <julian@jtle.dk>
2021-04-23 14:00:07 +0200

    Fix spelling mistakes and warnings

--- examples/awgn_rayleigh_channel_usage.py
+++ examples/awgn_rayleigh_channel_usage.py
@@ -18,11 +18,11 @@ import numpy as np
 # set random seed for getting same random returns
 np.random.seed(0)
 
-# construct a channel object called the channel, with N = 2 branches, snr
+# construct a channel object called the channel, with N = 2 branches, SNR
 # 0 [dB]
 channel = channel.RayleighAWGNChannel(N=2, snr=0)
 
-# print parameters just to see the other deafault values
+# print parameters just to see the other default values
 # the class instantly creates the first channel h parameters
 channel.print_parameters()
 
@@ -30,18 +30,18 @@ channel.print_parameters()
 test_sequence = np.array([1, 1, 1, 1, 1, 1, 1, 1], dtype=complex)
 print("test:", test_sequence)
 
-# run the test sequence throught the channel
+# run the test sequence through the channel
 hat_test_sequence = channel.run(test_sequence)
 
 # as there are two branches hat_test_sequence will return a 2 x
 # len(test_sequence) array (a matrix)
 print("hat_test_seqeunce\n", hat_test_sequence)
 
-# prints the samples of each branch independ
+# prints the samples of each branch independently
 print("hat_test_sequence[0]\n", hat_test_sequence[0])
 print("hat_test_sequence[1]\n", hat_test_sequence[1])
 
-# After the whole frame have been throught the channel, ,
+# After the whole frame have been through the channel,
 # call frame_sendt() to update the channel
 # The channel will automatically update to the next block
 # after 6 frames have been sendt
