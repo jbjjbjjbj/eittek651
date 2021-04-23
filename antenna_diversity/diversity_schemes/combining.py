2cdd7c320049c923ba26440ec2a7fb5292adde53
cspe18 <47355558+cspe18@users.noreply.github.com>
2021-04-23 13:53:37 +0200

    MRC changed to acommodate channel changes
    Co-Authored-By: hvisti <60644294+hvisti@users.noreply.github.com>

--- antenna_diversity/diversity_schemes/combining.py
+++ antenna_diversity/diversity_schemes/combining.py
@@ -8,27 +8,15 @@ from .. import channel
 #   Signal x(t) has unit average power
 #   SNR is instantaneous
 
-def MRC(input, SNR, nr_antenna):
-    x = input
-    alpha = channel.rayleigh(nr_antenna)
-    n = channel.AWGN(nr_antenna,SNR)
+def MRC(input, ray):
+    r = input
+    h = ray
     #Cauchy-Schwarz inequality dictates when the SNR is defined as:
-    # ((w hermitian alpha)^2)/σ2 the weight (w) has a maximum when it's
-    # linearly proportional to alpha (https://www.comm.utoronto.ca/~rsadve/Notes/DiversityReceive.pdf)
-    W = alpha
-    #We can because W=alpha and both being hermitian w H alpha = w^2
+    # ((w hermitian h)^2)/σ2 the weight (w) has a maximum when it's
+    # linearly proportional to h (https://www.comm.utoronto.ca/~rsadve/Notes/DiversityReceive.pdf)
+    W = np.transpose(h)
+    #We can because W=h and both being hermitian w H h = w^2
     #(https://www.gaussianwaves.com/2020/01/receiver-diversity-maximum-ratio-combining-mrc/)
-    #final result: y = w H y = w H alpha*x + w H n
-    h = np.transpose(W)
-    Wh = W.dot(h)
-    n = np.transpose(np.array([n]))
-    Wn = W.dot(n)
-
-    y = Wh*x+Wn
-    #Unfortunately it seems like my my complex conjugated and transpose method doesn't seem to work so
-    #the noise here is wrong but im unsure how to fix it.
+    #final result: y = w H y = w H h*x + w H n
+    y = W.dot(r)
     return(y)
-
-
-def EGC(input):
-    x=input
