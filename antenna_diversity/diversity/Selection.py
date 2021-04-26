52e4a9c0f1a7fe9f95f7f1fb2a0fc0cd4f8730cc
mikkelfb <mbengt17@student.aau.dk>
2021-04-26 08:26:57 +0200

    added-selection-diversity

---
+++ antenna_diversity/diversity/Selection.py
@@ -0,0 +1,17 @@
+import numpy as np
+import typing as t
+
+
+def selection(signal: np.ndarray,
+              channel_h: np.ndarray) -> t.Tuple[np.ndarray, int]:
+    """
+        Signal is a N x M signal where N is the number of branches and M is the signal places,
+        such for example that the signal[1] gives a array of M elements
+        channel_h is a array of the h impulse response for each branch of the channel
+
+        returns the signal y after selection and the index of the chosen h i.e. the chosen branch
+        """
+    # find the larges h and return the index (argument)
+    index = np.argmax(channel_h)
+    # return the signal at that index and the index.
+    return signal[index], index
---
+++ examples/selection_diversity.py
@@ -0,0 +1,15 @@
+import ad_path
+import numpy as np
+from antenna_diversity import diversity
+
+ad_path.nop()
+
+# contruct a channel h
+h = np.array([0.8, 1, 1.2])
+# create signal
+x = np.matrix('1 2 3 ; 4 5 6 ; 7 8 9')
+print('x:', x)
+
+y, index = diversity.Selection.selection(x, h)
+
+print('y:', y, '\n index:', index, '\n h:', h[index])
