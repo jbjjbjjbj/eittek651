8bb9b569c357f6657b02309343ac96357f48af1b
mikkelfb <mbengt17@student.aau.dk>
2021-04-26 09:03:44 +0200

    fixed-typing

--- antenna_diversity/diversity/Selection.py
+++ antenna_diversity/diversity/Selection.py
@@ -3,7 +3,7 @@ import typing as t
 
 
 def selection(signal: np.ndarray,
-              channel_h: np.ndarray) -> t.Tuple[np.ndarray, int]:
+              channel_h: np.ndarray) -> t.Tuple[t.Any, int]:
     """
         Signal is a N x M signal where N is the number of branches and M is the signal places,
         such for example that the signal[1] gives a array of M elements
@@ -14,4 +14,4 @@ def selection(signal: np.ndarray,
     # find the larges h and return the index (argument)
     index = np.argmax(channel_h)
     # return the signal at that index and the index.
-    return signal[index], index
+    return signal[index], int(index)
