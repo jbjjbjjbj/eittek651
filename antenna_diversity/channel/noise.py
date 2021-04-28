11a7098bc131f0a86d936bdddb35bbb34116a0b0
Julian T <julian@jtle.dk>
2021-04-28 11:17:16 +0200

    Multiple sigma to both components of AWGN_matrix noise

--- antenna_diversity/channel/noise.py
+++ antenna_diversity/channel/noise.py
@@ -14,19 +14,20 @@ from ..common import db_to_power
 
 def AWGN(nrElementOut, snr: float) -> np.ndarray:
     sigma = math.sqrt((1 / db_to_power(snr)) * 1 / 2)
-    W = np.random.standard_normal(
-        size=nrElementOut) * sigma + 1j * np.random.standard_normal(size=nrElementOut) * sigma
+    W = np.random.standard_normal(size=nrElementOut) * sigma + \
+        1j * np.random.standard_normal(size=nrElementOut) * sigma
     return W
 
 
-def AWGN_Matrix(rows: int, coloums: int, snr: float) -> np.ndarray:
+def AWGN_Matrix(rows: int, columns: int, snr: float) -> np.ndarray:
     """
-        Takes in number of rows and coloums
-        returns a matrix with size: rows x coloums with complex normal distributed varaibles
+        Takes in number of rows and columns
+        returns a matrix with size: rows x columns with complex normal
+        distributed variables
     """
-    W = np.empty(shape=(rows, coloums), dtype=complex)
+    W = np.empty(shape=(rows, columns), dtype=complex)
     sigma = math.sqrt((1 / db_to_power(snr)) * 1 / 2)
     for row in range(rows):
-        W[row] = np.random.standard_normal(
-            size=coloums) * sigma + 1j * np.random.standard_normal(size=coloums)
+        W[row] = np.random.standard_normal(size=columns) * sigma + \
+            1j * np.random.standard_normal(size=columns) * sigma
     return W
