1afd7fbeb2d1a87cdf6fb0a79b0097763e20dacf
Julian T <julian@jtle.dk>
2021-04-30 11:05:45 +0200

    Use linestyles for different branches, and correcly name branches

--- examples/diversity_graph_draw.py
+++ examples/diversity_graph_draw.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import re
 import h5py
 import matplotlib.pyplot as plt
+import matplotlib
 
 diversity_file_re = re.compile("diversity_(.*)\\.h5$")
 output_default = "diversity_all.pdf"
+
+styles = [":", "-.", "--", "-"]
+# https://matplotlib.org/stable/gallery/color/named_colors.html
+colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple",
+          "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
 
 # Find diversity files in current dir
 data_files = []
@@ -30,7 +36,7 @@ if output == "":
 
 
 # Load each data thing and plot it
-for choise in choises:
+for color_i, choise in enumerate(choises):
     fname, name = data_files[choise]
     print(f"Loading file {fname}")
     with h5py.File(fname, "r") as f:
@@ -44,10 +50,12 @@ for choise in choises:
     if branches == 1:
         label = [name]
     else:
-        label = [f"Branch {i}({name})" for i in range(branches)]
+        label = [f"Branch {i+1}({name})" for i in range(branches)]
 
-    for i in range(branches):
-        plt.plot(snr, prob[i], label=label[i])
+    # TODO hmm we are limited by the number of line styles, that is stupid
+    for i in range(min(branches, len(styles))):
+        plt.plot(snr, prob[i], label=label[i], linestyle=styles[i],
+                 color=colors[color_i])
 
 plt.yscale("log")
 plt.ylabel("Symbol Error Probability")
