450003d7a8a2810156aeefe9e04b9b1235320842
Julian T <julian@jtle.dk>
2021-05-03 15:24:53 +0200

    Make diversity_graph_draw fit the graphs in report

--- examples/diversity_graph_draw.py
+++ examples/diversity_graph_draw.py
@@ -2,15 +2,9 @@ from pathlib import Path
 import re
 import h5py
 import matplotlib.pyplot as plt
-import matplotlib
 
 diversity_file_re = re.compile("diversity_(.*)\\.h5$")
 output_default = "diversity_all.pdf"
-
-styles = [":", "-.", "--", "-"]
-# https://matplotlib.org/stable/gallery/color/named_colors.html
-colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple",
-          "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
 
 # Find diversity files in current dir
 data_files = []
@@ -25,12 +19,15 @@ print("Found data files:")
 for i, (_, name) in enumerate(data_files):
     print(f"  {i}: {name}")
 
-choise_str = input("Select by numbers (use space for seperation): ")
-choises = [int(s) for s in choise_str.split(" ")]
+choise_str = input("Select dataset by number: ")
+choise = int(choise_str)
 
 sel_branches_str = input("Select the wanted branches \
-(use space for seperation): ")
-sel_branches = [int(s) for s in sel_branches_str.split(" ")]
+(use space for seperation, nothing for all): ")
+if sel_branches_str != "":
+    sel_branches = [int(s) for s in sel_branches_str.split(" ")]
+else:
+    sel_branches = None
 
 title = input("Write a nice title for the graph(leave empty if unwanted): ")
 
@@ -38,33 +35,40 @@ output = input(f"Where to save (leave empty for {output_default}): ")
 if output == "":
     output = output_default
 
-
 # Load each data thing and plot it
-for color_i, choise in enumerate(choises):
-    fname, name = data_files[choise]
-    print(f"Loading file {fname}")
-    with h5py.File(fname, "r") as f:
-        snr = f["snrs"][:]
-        prob = f["probs"][:]
+fname, name = data_files[choise]
+print(f"Loading file {fname}")
+with h5py.File(fname, "r") as f:
+    snr = f["snrs"][:]
+    prob = f["probs"][:]
 
-    branches = len(prob)
-    print(f"Found {branches} branches")
+branches = len(prob)
+print(f"Found {branches} branches")
 
-    # If there are no branches we should only include the name in the legend
-    if len(sel_branches) == 1:
-        label = [name]
-    else:
-        label = [f"Branch {i+1}({name})" for i in sel_branches]
+if sel_branches is None:
+    sel_branches = range(branches)
 
-    # TODO hmm we are limited by the number of line styles, that is stupid
-    for i, b in enumerate(sel_branches):
-        plt.plot(snr, prob[b], label=label[i], linestyle=styles[i],
-                 color=colors[color_i], lw=1)
+legends = []
 
+# TODO hmm we are limited by the number of line styles, that is stupid
+for i, b in enumerate(sel_branches):
+    plt.plot(snr, prob[b])
+    legends.append(f"N = {b}")
+
+print(legends)
+
+plt.legend(legends)
 plt.yscale("log")
-plt.ylabel("Symbol Error Probability")
+plt.ylabel("Symbol Error Rate")
 plt.xlabel("SNR [dB]")
-plt.legend()
+plt.grid(True)
+# format axis to bigger to bigger font
+plt.rc('font', size=20)  # controls default text size
+plt.rc('axes', titlesize=20)  # fontsize of the title
+plt.rc('axes', labelsize=20)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=20)  # fontsize of the x tick labels
+plt.rc('ytick', labelsize=20)  # fontsize of the y tick labels
+plt.rc('legend', fontsize=20)  # fontsize of the legend
 if title != "":
     print(f"Setting title '{title}'")
     plt.title(title)
