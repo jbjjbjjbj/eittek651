281b8265a57c0c09c874fd41c981e89ab71ca373
Julian T <julian@jtle.dk>
2021-05-03 11:27:29 +0200

    Fix Jan comments on figure

--- examples/gfsk_awgn.py
+++ examples/gfsk_awgn.py
@@ -3,6 +3,7 @@ import ad_path
 from antenna_diversity import modulation, channel
 import matplotlib.pyplot as plt
 import h5py
+import sys
 
 ad_path.nop()
 
@@ -22,32 +23,45 @@ def run_gfsk_simulation(snr):
     return wrong, n
 
 
-snr = np.arange(-5, 16, 2)
+def generate_data():
+    snr = np.arange(-5, 16, 2)
 
-prob = np.empty(len(snr))
-for i, gamma in enumerate(snr):
-    print(gamma)
-    w_t, n = run_gfsk_simulation(snr=gamma)
-    num_wrong = w_t
-    number = n
-    while(num_wrong < 1000 and number < chunkSize*1000):
+    prob = np.empty(len(snr))
+    for i, gamma in enumerate(snr):
+        print(gamma)
         w_t, n = run_gfsk_simulation(snr=gamma)
-        num_wrong += w_t
-        number += n
-    prob[i] = (num_wrong / number)
+        num_wrong = w_t
+        number = n
+        while num_wrong < 1000 and number < chunkSize*1000:
+            w_t, n = run_gfsk_simulation(snr=gamma)
+            num_wrong += w_t
+            number += n
+        prob[i] = (num_wrong / number)
+
+    with h5py.File("gfsk_awgn.h5", "w") as f:
+        f.create_dataset("probs", data=prob)
+        f.create_dataset("snrs", data=snr)
+
+    return snr, prob
+
+
+# If a second argument is given load from existing file
+if len(sys.argv) > 1:
+    with h5py.File("gfsk_awgn.h5", "r") as f:
+        snr = f["snrs"][:]
+        prob = f["probs"][:]
+else:
+    snr, prob = generate_data()
 
 
 book_snr = np.array([0, 2, 4, 6, 8, 10, 12])
 book_prob = np.array([0.2, 0.1, 0.08, 0.03, 0.007, 0.0009, 0.00002])
 
-with h5py.File("gfsk_awgn.h5", "w") as f:
-    f.create_dataset("probs", data=prob)
-    f.create_dataset("snrs", data=snr)
 
 plt.plot(snr, prob)
 plt.scatter(book_snr, book_prob, color='r')
-plt.xlabel("snr [dB]")
-plt.ylabel("Error probability")
-plt.legend(["Simulation", "Viswanathan"])
-plt.semilogy()
-plt.savefig("out.png")
+plt.xlabel("SNR [dB]")
+plt.ylabel("Symbol Error Probability")
+plt.legend(["Simulation", "Reference"])
+plt.yscale("log")
+plt.savefig("gfsk_awgn.pdf")
