3bb1c71a186489efa98e49175670520e5aaac072
Julian T <julian@jtle.dk>
2021-05-04 13:35:51 +0200

    Catch floating point error in constellmod.py
    As mentioned in #55

--- test/test_constellmod.py
+++ test/test_constellmod.py
@@ -5,6 +5,7 @@ import unittest
 import antenna_diversity.modulation as modulation
 import numpy as np
 
+
 class TestConstellationModulator(unittest.TestCase):
     def test_overall(self):
         m = modulation.PSK(4)
@@ -13,3 +14,13 @@ class TestConstellationModulator(unittest.TestCase):
         demodulated = m.demodulate(modulated)
         self.assertTrue(np.array_equal(syms, demodulated))
         self.assertTrue(demodulated.dtype == syms.dtype)
+
+    def test_floating_input(self):
+        m = modulation.PSK(4)
+        syms = np.random.randint(0, 4, size=10).astype(float)
+
+        try:
+            m.modulate(syms)
+        except IndexError:
+            self.fail("IndexError on symbol modulation, could not handle \
+floating point")
