6e564e5703c00613a21624732f265bde3bbeca56
mikkelfb <mbengt17@student.aau.dk>
2021-05-05 13:55:47 +0200

    Edited-selection-diveristy-to-work-with-power
    Updated selection diversity to use power instead of h
    Kept the old for rapport reasons

--- antenna_diversity/diversity_technique/selection.py
+++ antenna_diversity/diversity_technique/selection.py
@@ -2,7 +2,7 @@ import numpy as np
 import typing as t
 
 
-def selection(signal: np.ndarray,
+def selection_from_h(signal: np.ndarray,
               channel_h: np.ndarray) -> t.Tuple[t.Any, int]:
     """
         Signal is a N x M signal where N is the number of branches and M is the signal places,
@@ -15,3 +15,40 @@ def selection(signal: np.ndarray,
     index = np.argmax(channel_h)
     # return the signal at that index and the index.
     return signal[index], int(index)
+
+
+def calculate_power(signal_array: np.ndarray, numb_bits, over_sample_rate):
+    # first cut out numb_bits of signal_array.
+    # the typical physical implementation of selection only uses x number bits
+    # using same deafault of 4 bits, and oversampelrate of 32 as the deafault
+    # for gfsk is set to 32
+    # Calculation is from page 160 in wireless communication systems in matlab
+    signal_part = signal_array[0: int(numb_bits * over_sample_rate)]
+    signal_abs_sqaured = np.square(np.abs(signal_part))
+    return 1 / len(signal_part) * np.sum(signal_abs_sqaured)
+
+
+def selection_from_power(signal_matrix: np.ndarray,
+                         numb_bits=4, over_sample_rate=32) -> t.Tuple[t.Any, int]:
+    """
+        Signal_matrix is a N x M signal where N is the number of branches and M is the signal places,
+        such for example that the signal[1] gives a array of M elements
+
+        This function uses the energy of the signal to choose which branch to use
+        numb_bits is the amount of bits to use for calculating the signal energy
+        over_sample_rate is the oversamplerate used to make the baseband representation in the gfsk module
+
+        numb_bits = 4 is set as it is the common number of bits used in physical devices
+        over_sample_rate = 32 is the same deafault as the gfsk module
+
+        returns the signal y after selection and the index of the chosen h i.e. the chosen branch
+    """
+    sh = np.shape(signal_matrix)
+    signal_powers = np.empty(shape=(sh[0]))
+    for i, signal_row in enumerate(signal_matrix):
+        signal_powers[i] = calculate_power(
+            signal_row,
+            numb_bits=numb_bits,
+            over_sample_rate=over_sample_rate)
+    index = np.argmax(signal_powers)
+    return signal_matrix[index], int(index)
--- test/test_selection.py
+++ test/test_selection.py
@@ -1,4 +1,8 @@
 import unittest
+from unittest import signals
+from numpy.core.fromnumeric import shape
+
+from scipy.signal.signaltools import resample
 from antenna_diversity.diversity_technique import selection
 import numpy as np
 
@@ -6,11 +10,18 @@ import numpy as np
 class TestSelection(unittest.TestCase):
     def setUp(self):
         self.signals = np.array([[1, 2, 3], [4, 5, 2], [0, 0, 0]])
+        self.signals_from_power = np.array(
+            [np.ones(shape=32 * 4), np.ones(shape=32 * 4) * 0.5, np.ones(shape=32 * 4) * 2])
 
     def test_simple(self):
         hs = np.array([0, -100, 2])
         chosen = 2
-
-        res, index = selection(self.signals, hs)
+        res, index = selection.selection_from_h(self.signals, hs)
         self.assertEqual(chosen, index)
         np.testing.assert_array_equal(res, self.signals[chosen])
+
+    def test_simple_from_power(self):
+        chosen = 2
+        res, index = selection.selection_from_power(self.signals_from_power)
+        self.assertEqual(chosen, index)
+        np.testing.assert_array_equal(res, self.signals_from_power[chosen])
