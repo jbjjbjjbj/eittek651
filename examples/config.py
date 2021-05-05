52baf569030e509ebccc302983e20ebde00f8ddf
Julian T <julian@jtle.dk>
2021-05-05 14:15:21 +0200

    First draft for rework

---
+++ examples/config.py
@@ -0,0 +1,21 @@
+
+class Collection:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+ber_over_snr = Collection(
+    bits_per_slot=440,
+    slot_per_frame=1,
+    give_up_value=1e-6,
+    # How many bits to aim for at give_up_value
+    certainty=20,
+    # Stop early at x number of errors. Make sure to scale together with
+    # slots_per_frame, as this number number must include several different
+    # h values.
+    stop_at_errors=100000,
+    snr_stop=100,
+    snr_step=2.5,
+    branches=5
+)
+
--- examples/selection_diversity_graph.py
+++ examples/selection_diversity_graph.py
@@ -3,9 +3,9 @@ import ad_path
 from antenna_diversity import modulation, channel, diversity_technique, common
 import matplotlib.pyplot as plt
 import h5py
+import config.ber_over_snr as cfg
 
-bits_per_slot = 440
-slot_per_frame = 1
+ad_path.nop()
 
 gfsk = modulation.GFSK()
 
@@ -14,30 +14,34 @@ gfsk = modulation.GFSK()
 
 
 def make_frame_array():
-    frameArray = np.empty(shape=(slot_per_frame, bits_per_slot))
-    for i in range(slot_per_frame):
-        frameArray[i] = np.random.randint(2, size=bits_per_slot)
+    frameArray = np.empty(shape=(cfg.slot_per_frame, cfg.bits_per_slot))
+    for i in range(cfg.slot_per_frame):
+        frameArray[i] = np.random.randint(2, size=cfg.bits_per_slot)
     return frameArray
 
 
-snr = np.arange(-10, 17.5, 2.5)
+snr_todo = np.arange(-10, cfg.snr_stop+cfg.snr_step, cfg.snr_step)
 
-branches = np.arange(1, 6, 1)
-prob = np.empty(shape=(len(branches), len(snr)))
-base_tries = 5000
+bit_goal = np.ceil(1/cfg.give_up_value) * cfg.certainty
+max_tries = int(np.ceil(bit_goal / (cfg.bits_per_slot * cfg.slot_per_frame)))
+
+branches = np.arange(1, cfg.branches+1, 1)
+
+shape = (len(branches), len(snr_todo))
+# nan or "not a number" values are ignored when plotting
+probs = np.full(shape, np.nan)
+snrs = np.full(shape, np.nan)
 
 for j, branch in enumerate(branches):
-    # tries is the number of runs
-    tries = base_tries
-    for i, gamma in enumerate(snr):
+    branch_probs = []
+    branch_snrs = []
+    for i, gamma in enumerate(snr_todo):
         # create channel with the curretn branch and snr
         ch = channel.RayleighAWGNChannel(N=branch, snr=gamma)
         errors = 0
-        numberOfTries = 0
-        # make it run extra tries if the snr is bigger then zero
-        if gamma > 0:
-            tries += 1000
-        for k in range(tries):
+        numberOfBits = 0
+
+        for k in range(max_tries):
             fram = make_frame_array()
             for slot in fram:
                 signal = gfsk.modulate(slot)
@@ -46,23 +50,35 @@ for j, branch in enumerate(branches):
                 hat_slot = gfsk.demodulate(hat_recieved)
                 err, n = common.count_symbol_errors(slot, hat_slot)
                 errors += err
-                numberOfTries += n
+                numberOfBits += n
             ch.frame_sent()
-        prob[j][i] = errors / numberOfTries
-        print('snr', gamma, 'branch', branch, 'Prob:', prob[j][i])
 
+            if errors >= cfg.stop_at_errors:
+                print(f"Stopping early at {k} runs")
+                break
+
+        ber = errors / numberOfBits
+        print('snr', gamma, 'branch', branch, 'Prob:', ber)
+
+        snrs[j][i] = gamma
+        probs[j][i] = ber
+
+        # Stop when below give_up_value
+        if ber < cfg.give_up_value:
+            print(f"Under {cfg.give_up_value}, stopping here")
+            break
 
 legends = []
 
 for i, branch in enumerate(branches):
-    plt.plot(snr, prob[i])
+    plt.plot(snrs[i], probs[i])
     s = 'N = '
     s += str(branch)
     legends.append(s)
 
 with h5py.File("diversity_selection.h5", "w") as f:
-    f.create_dataset("probs", data=prob)
-    f.create_dataset("snrs", data=snr)
+    f.create_dataset("probs", data=probs)
+    f.create_dataset("snrs", data=snrs)
 
 plt.legend(legends)
 plt.yscale('log')
