500de397cc845907a120023f367343549632d92e
mikkelfb <mbengt17@student.aau.dk>
2021-05-12 21:53:32 +0200

    Create channel_h_comparison_with_one_log.py

---
+++ examples/channel_h_comparison_with_one_log.py
@@ -0,0 +1,139 @@
+import ad_path
+from antenna_diversity.channel import channel_models
+from antenna_diversity import channel
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def read_data():
+    file = open('logs/Basestation_walk_niels_jernes_library.log', 'r')
+    lines = file.read().splitlines()
+    file.close()
+    return lines
+
+
+lines = read_data()
+
+rssi_str = np.empty(shape=(len(lines) - 1))
+time_from_zero = np.arange(0, len(lines) * 10 - 10, 10)
+time_str = []
+sync_str = []
+a_crc_str = []
+x_crc_str = []
+z_crc_str = []
+q1_str = []
+q2_str = []
+ant_str = []
+pow_Str = []
+
+for i, line in enumerate(lines):
+    if(i == 0):
+        print(line)
+    else:
+        line_splited = line.split()
+        # print(line_splited)
+        time_str.append(line_splited[0])
+        rssi_str[i - 1] = float(line_splited[1])
+        sync_str.append(line_splited[2])
+        a_crc_str.append(line_splited[3])
+        x_crc_str.append(line_splited[4])
+        z_crc_str.append(line_splited[5])
+        q1_str.append(line_splited[6])
+        q2_str.append(line_splited[7])
+        ant_str.append(line_splited[8])
+        pow_Str.append(line_splited[9])
+
+rssi_sixty_gns = np.empty_like(rssi_str)
+diff_from_gns = np.empty_like(rssi_str)
+
+counter = 0
+sum_rssi = 0
+
+for i, rssi in enumerate(rssi_str):
+    diff_from_gns[i] = rssi_sixty_gns[i] - rssi
+
+data_formodlling = rssi_str[200:400]
+x_value_modeling = time_from_zero[200:400]
+data_formodlling_gns = np.mean(data_formodlling)
+data_formodling_mdian = np.median(data_formodlling)
+data_formodlling_norm = data_formodlling - data_formodlling_gns
+mean_line = np.ones(shape=len(data_formodlling)) * data_formodlling_gns
+median_line = np.ones(shape=len(data_formodlling)) * data_formodling_mdian
+
+print(data_formodlling_gns)
+
+
+ch_1 = channel.channel_models.RayleighAWGNChannel(1, 0)
+ch_2 = channel_models.RayleighAWGNChannel(
+    1, 0, frame_per_block=3, intermediate_point=False)
+ch_3 = channel_models.RayleighAWGNChannel(
+    1, 0, frame_per_block=4)
+ch_old = channel_models.RayleighAWGNChannelOld(1, 0)
+
+h_1 = np.empty(shape=(len(data_formodlling)))
+h_2 = np.empty(shape=(len(data_formodlling)))
+h_3 = np.empty(shape=(len(data_formodlling)))
+h_old = np.empty(shape=(len(data_formodlling)))
+
+signal = np.ones(1)
+for i in range(len(data_formodlling)):
+    _, h_from_ch1 = ch_1.run(signal)
+    h_1[i] = 10 * math.log10(h_from_ch1)
+    _, h_from_ch2 = ch_2.run(signal)
+    h_2[i] = 10 * math.log10(h_from_ch2)
+    _, h_from_h3 = ch_3.run(signal)
+    h_3[i] = 10 * math.log10(h_from_h3)
+    _, h_from_chold = ch_old.run(signal)
+    h_old[i] = 10 * math.log10(h_from_chold)
+    ch_1.frame_sent()
+    ch_2.frame_sent()
+    ch_3.frame_sent()
+    ch_old.frame_sent()
+
+#plt.plot(x_value_modeling[0:len(data_sigma_upsample)], data_sigma_upsample_db)
+#plt.plot(x_value_modeling[0:len(data_sigma_upsample)], data_sqrt_db)
+
+
+fig, axs = plt.subplots(4, sharex=True, sharey=True)
+
+axs[0].plot(x_value_modeling, h_1, linewidth=1,
+            linestyle='-', color='#0072BD')
+
+axs[0].plot(x_value_modeling, data_formodlling_norm, linewidth=1,
+            linestyle='--', color='#A2142F')
+
+axs[1].plot(x_value_modeling, h_2, linewidth=1,
+            linestyle='-', color='#77AC30')
+
+axs[1].plot(x_value_modeling, data_formodlling_norm, linewidth=1,
+            linestyle='--', color='#A2142F')
+
+axs[2].plot(x_value_modeling, h_3, linewidth=1,
+            linestyle='-', color='#EDB120')
+
+axs[2].plot(x_value_modeling, data_formodlling_norm, linewidth=1,
+            linestyle='--', color='#A2142F')
+
+axs[3].plot(x_value_modeling, h_old, linewidth=1,
+            linestyle='-', color='#7E2F8E')
+
+axs[3].plot(x_value_modeling, data_formodlling_norm, linewidth=1,
+            linestyle='--', color='#A2142F')
+# print('sigma',sigam_square)
+
+axs[0].legend(['Channel h with intermediate',
+              'Library data'], fontsize='xx-small', loc='lower right')
+axs[1].legend(['Channel h without intermediate',
+              'Library data'], fontsize='xx-small', loc='lower right')
+axs[2].legend(['Channel h with intermediate 4 frame Tc',
+              'Library data'],
+              fontsize='xx-small', loc='lower right')
+axs[3].legend(['Old channel h', 'Library data'],
+              fontsize='xx-small', loc='lower right')
+
+
+plt.xlabel('Time [ms]')
+plt.ylabel('RSSI [dBm]')
+plt.savefig('New_old_channel_model_with_lib_data.pdf')
+plt.show()
